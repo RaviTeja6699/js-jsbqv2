e72f713133231ac282476c4c13907ca6004776f2
RaviTeja6699 <vaggaraviteja63@gmail.com>
2021-11-06 06:10:54 +0530

    1 conditoanal st

--- index.js
+++ index.js
@@ -4,3 +4,91 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// conditional statements
+/*var a=1;
+var b=2;
+if (a<b){
+  console.log("A is less than B");
+ // document.write('A is less than B');
+}
+else{
+ // document.write('B is grater tahn A');
+  console.log('B is grater than A')
+}
+*/
+// number divisible by 7
+//remainder has to zero so we need to use %
+/*var c=prompt();
+if (c%7===0){
+  console.log('it is divisble by 7');
+}
+else{
+  console.log('it is not divisible by 7');
+}
+*/
+
+/*
+//divisble by seven or not and check it is number or not
+var c=prompt();
+ var res=c%7;
+if (isNaN(res) ){
+  console.log('it is NaN ');
+  alert('plase enter valid number')
+}
+else{
+  
+  if (res===0){
+    console.log('it is divisble by 7');
+  }
+  else{
+    console.log('it is not divisible by 7');
+  }
+
+}
+*/
+
+// given number is even or odd
+/*
+var c=prompt();
+if (c%2===0){
+  console.log('it is even');
+}
+else{
+  console.log('it is not even');
+}
+*/
+
+
+var d=1;
+while (d<=10)
+{
+  console.log(d);
+  d=d+1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
